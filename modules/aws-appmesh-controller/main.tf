0e1d5615d6865d90de8f36c10150dfc67613acda
git4suvendu <m_andal2002@yahoo.co.in>
2022-06-19 07:47:38 +0100

    Test appmesh controller

---
+++ modules/aws-appmesh-controller/main.tf
@@ -0,0 +1,147 @@
+locals {
+  appmesh_controller_helm_repo     = "https://aws.github.io/eks-charts"
+  appmesh_controller_chart_name    = "appmesh-controller"
+  alb_controller_chart_version = var.aws_appmesh_controller_chart_version
+  aws_vpc_id                   = data.aws_vpc.selected.id
+  aws_region_name              = data.aws_region.current.name
+}
+
+resource "aws_iam_role" "this" {
+  name        = substr("${var.k8s_cluster_name}-aws-appmesh-controller", 0, 64)
+  description = "Permissions required by the Kubernetes AWS Appmesh controller to do its job."
+  path        = ""
+  tags = var.aws_tags
+  force_detach_policies = true
+  assume_role_policy = data.aws_iam_policy_document.eks_oidc_assume_role[0].json
+}
+
+
+resource "aws_iam_policy" "this" {
+  name        = substr("${var.k8s_cluster_name}-appmesh-controller-management",0,64)
+  description = format("Permissions that are required to manage AWS Appmesh.")
+  path        = "/"
+  # We use a heredoc for the policy JSON so that we can more easily diff and
+  # copy/paste from upstream. Ignore whitespace when you diff to more easily see the changes!
+  # Source: `curl -o controller-iam-policy.json https://raw.githubusercontent.com/aws/aws-app-mesh-controller-for-k8s/master/config/iam/controller-iam-policy.json`
+  policy = file("${path.module}/aws_appmesh_iam_policy.json")
+        tags = {
+                 Name        = "${var.k8s_cluster_name}-aws-appmesh-controller-iam"
+                }
+  }
+
+resource "aws_iam_role_policy_attachment" "this" {
+  policy_arn = aws_iam_policy.this.arn
+  role       = aws_iam_role.this.name
+}
+
+resource "kubernetes_service_account" "this" {
+  automount_service_account_token = true
+  metadata {
+    name      =  substr("${var.k8s_cluster_name}-${var.environment}-aws-appmesh-controller",0,64)
+    namespace = var.k8s_namespace
+    annotations = {
+      # This annotation is only used when running on EKS which can
+      # use IAM roles for service accounts.
+      "eks.amazonaws.com/role-arn" = aws_iam_role.this.arn
+    }
+    labels = {
+      "app.kubernetes.io/name"       = substr("${var.k8s_cluster_name}-${var.environment}-aws-appmesh-controller",0,64)
+      "app.kubernetes.io/component"  = "controller"
+      "app.kubernetes.io/managed-by" = "terraform"
+    }
+  }
+}
+
+resource "kubernetes_cluster_role" "this" {
+  metadata {
+    name = substr("${var.k8s_cluster_name}-${var.environment}-aws-appmesh-controller",0,64)
+
+    labels = {
+      "app.kubernetes.io/name"       = substr("${var.k8s_cluster_name}-${var.environment}-aws-appmesh-controller",0,64)
+      "app.kubernetes.io/managed-by" = "terraform"
+    }
+  }
+  rule {
+    api_groups = [
+      "",
+      "extensions",
+    ]
+
+    resources = [
+      "nodes",
+      "pods",
+      "secrets",
+      "services",
+      "namespaces",
+    ]
+
+    verbs = [
+      "get",
+      "list",
+      "watch",
+    ]
+  }
+}
+
+resource "kubernetes_cluster_role_binding" "this" {
+  metadata {
+    name = substr("${var.k8s_cluster_name}-${var.environment}-aws-appmesh-controller",0,64)
+
+    labels = {
+      "app.kubernetes.io/name"       = substr("${var.k8s_cluster_name}-${var.environment}-aws-appmesh-controller",0,64)
+      "app.kubernetes.io/managed-by" = "terraform"
+    }
+  }
+
+  role_ref {
+    api_group = "rbac.authorization.k8s.io"
+    kind      = "ClusterRole"
+    name      = kubernetes_cluster_role.this.metadata[0].name
+  }
+
+  subject {
+    api_group = ""
+    kind      = "ServiceAccount"
+    name      = kubernetes_service_account.this.metadata[0].name
+    namespace = kubernetes_service_account.this.metadata[0].namespace
+  }
+}
+
+resource "helm_release" "alb_controller" {
+
+  name       = "appmesh-controller"
+  repository = local.appmesh_controller_helm_repo
+  chart      = local.appmesh_controller_chart_name
+  version    = local.alb_controller_chart_version
+  namespace  = var.k8s_namespace
+  create_namespace = true
+  atomic     = true
+  timeout    = 900
+  cleanup_on_fail = true
+
+  dynamic "set" {
+
+    for_each = {
+      "clusterName"           = var.k8s_cluster_name
+      "serviceAccount.create" = "false"
+      "serviceAccount.name"   =   kubernetes_service_account.this.metadata[0].name
+      "region"                = local.aws_region_name
+      "vpcId"                 = local.aws_vpc_id
+      "hostNetwork"           = var.enable_host_networking
+    }
+    content {
+      name  = set.key
+      value = set.value
+    }
+  }
+
+  dynamic "set" {
+    for_each = var.chart_env_overrides
+    content {
+      name  = set.key
+      value = set.value
+    }
+  }
+
+}
+
---
+++ modules/aws-appmesh-controller/variable.tf
@@ -0,0 +1,52 @@
+
+
+variable "k8s_cluster_name" {
+  description = "Name of the Kubernetes cluster. This string is used to contruct the AWS IAM permissions and roles. If targeting EKS, the corresponsing managed cluster name must match as well."
+  type        = string
+}
+
+variable "k8s_namespace" {
+  description = "Kubernetes namespace to deploy the AWS Load Balancer Controller into."
+  type        = string
+  default     = "default"
+}
+
+variable "aws_vpc_id" {
+  description = "ID of the Virtual Private Network to utilize. Can be ommited if targeting EKS."
+  type        = string
+  default     = null
+}
+
+variable "aws_region_name" {
+  description = "ID of the Virtual Private Network to utilize. Can be ommited if targeting EKS."
+  type        = string
+  default     = null
+}
+
+variable "aws_tags" {
+  description = "Common AWS tags to be applied to all AWS objects being created."
+  type        = map(string)
+  default     = {}
+}
+
+variable "aws_appmesh_controller_chart_version" {
+  description = "The AWS AppMesh Controller version to use. See https://github.com/aws/eks-charts/releases/ and https://github.com/aws/aws-app-mesh-controller-for-k8s/releases for available versions"
+  type        = string
+  default     = "1.5.0"
+}
+
+variable "enable_host_networking" {
+  description = "If true enable host networking. See https://github.com/aws/eks-charts/tree/master/stable/aws-load-balancer-controller#configuration for details."
+  type        = bool
+  default     = false
+}
+
+variable "chart_env_overrides" {
+  description = "env values passed to the load balancer controller helm chart."
+  type        = map(any)
+  default     = {}
+}
+
+variable "environment" {
+  default = ""
+}
