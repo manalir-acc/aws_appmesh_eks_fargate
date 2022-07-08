42497db1f69f58b67a014972a7256d62b64fb6c0
git4suvendu <m_andal2002@yahoo.co.in>
2022-07-08 15:47:52 +0100

    fluentbit test3

--- main.tf
+++ main.tf
@@ -81,15 +81,20 @@ module "aws_appmesh_controller" {
 
 
 
-
+/*
 module "kubernetes_app" {
     source                      =  "./modules/kubernetes-app"
     app_namespace               =  var.fargate_app_namespace[0]
 
   depends_on = [module.eks, module.aws_alb_controller]
 }
+*/
 
 
+module "kubernetes_app_helm" {
+    source                      =  "./modules/kubernetes-app-helm"
+    app_namespace               =  var.fargate_app_namespace[0]
 
-
+  depends_on = [module.eks, module.aws_alb_controller]
+}
 
