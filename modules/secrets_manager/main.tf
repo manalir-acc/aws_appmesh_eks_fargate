3ba388caf9b18d1fd2b2f58db0217b035cd873f0
git4suvendu <m_andal2002@yahoo.co.in>
2022-07-13 09:31:43 +0100

    test3

--- modules/secrets_manager/main.tf
+++ modules/secrets_manager/main.tf
@@ -4,7 +4,7 @@
 
 locals {
   external_secrets_helm_repo     = "https://charts.external-secrets.io"
-  external_secrets_chart_name    = "external-secrets/external-secrets"
+  external_secrets_chart_name    = "external-secrets"
   aws_vpc_id                   = data.aws_vpc.selected.id
   aws_region_name              = data.aws_region.current.name
   external_secrets_chart_version = "0.5.8"
